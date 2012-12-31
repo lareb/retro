245c0cd05b43d9796c8120ddf044b0b7ef3f8428
lareb <lareb.indore@gmail.com>
2012-12-31 13:35:46 +0530

    Pathshala:: 1st commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pathshala::Application.config.secret_token = 'e26560c73098190e23c3a8bcbe3ad277594b40b46c3b8b744f18324e7f62c6980d899202b592eb5df35d3516dcfc976989b67ce56dd201806e93bf82354fcda4'
