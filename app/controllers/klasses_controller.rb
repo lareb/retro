8ebad5151ef2912cb3902a3c06a76f499ba8c972
lareb <lareb.indore@gmail.com>
2013-01-02 10:42:46 +0530

    update KlassController

--- app/controllers/klasses_controller.rb
+++ app/controllers/klasses_controller.rb
@@ -1,11 +1,12 @@
 class KlassesController < ApplicationController
   before_filter :authenticate_user!
-
+  load_and_authorize_resource
+  
   # GET /klasses
   # GET /klasses.json
   def index
-    @klasses = Klass.all
-
+    #@klasses = Klass.all
+    #load_and_authorize_resource will load the object
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @klasses }
@@ -16,8 +17,8 @@ class KlassesController < ApplicationController
   # GET /students/1.json
   # nishant test
   def show
-    @klass = Klass.find(params[:id])
-
+    #@klass = Klass.find(params[:id])
+    #load_and_authorize_resource will load the object
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @klass }
