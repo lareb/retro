94a5ea193ee4581570a67e478cacca771c702bd3
lareb <lareb.indore@gmail.com>
2013-06-02 14:08:50 +0530

    UI Changes, looks awesome, yooo!!!!

--- db/migrate/20121231085104_create_klasses.rb
+++ db/migrate/20121231085104_create_klasses.rb
@@ -1,7 +1,7 @@
 class CreateKlasses < ActiveRecord::Migration
   def change
     create_table :klasses do |t|
-      t.string  :name, :null => false, :limit => 4
+      t.string  :name, :null => false, :limit => 45
       t.string  :display_name, :null => false, :limit => 45
       t.integer :max_studen_count
       t.timestamps
