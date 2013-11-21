1ee080709e205022105ea7c7f415c8d7a816f48c
lareb <lareb.indore@gmail.com>
2013-11-21 23:32:36 +0530

    Asset defaul image and styling

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,7 +1,7 @@
 class Asset < ActiveRecord::Base
   attr_accessible :name, :description, :data
-  has_attached_file :data, :styles => {:small => "150x150>", :thumb => {:geometry => "100x100>"}},
-                    :whiny => false #,
+  has_attached_file :data, :styles => {:small => "200x175>", :thumb => {:geometry => "50x44>"}},
+                    :whiny => false, :default_url => "/assets/web/default-stdnt.jpg"
                     #:processors => [:padder]
   #has_attached_file :data, :styles => { :small => "150x150>" }, :default_url => "/assets/msri/default_institution_logo.png"
   belongs_to :attachable, :polymorphic => true
